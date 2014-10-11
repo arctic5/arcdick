67b8641f4c7d77bef9b4a3cd0ba9e741926351e2
arctic <icyfreeze5@gmail.com>
2014-10-10 23:53:37 -0700

    wat

--- bot.py
+++ bot.py
@@ -59,29 +59,6 @@ ignore = []
 
 readbuffer=""
 
-m8ball = []
-m8ball.append("Don't count on it")
-m8ball.append('My reply is no')
-m8ball.append('It is certain')
-m8ball.append('Outlook not so good')
-m8ball.append('Most likely')
-m8ball.append('Concentrate and ask again')
-m8ball.append('Yes definitely')
-m8ball.append('Without a doubt')
-m8ball.append('It is decidedly so')
-m8ball.append('My sources say no')
-m8ball.append('Signs point to yes')
-m8ball.append('Reply hazy try again')
-m8ball.append('Better not tell you now')
-m8ball.append('Very doubtful')
-m8ball.append('Cannot predict now')
-m8ball.append('You may rely on it')
-m8ball.append('Ask again later')
-m8ball.append('As I see it, yes')
-m8ball.append('Yes')
-m8ball.append('Outlook good')
-
-
 def send(msg):
     line = "%s\r\n" % msg
     try:
